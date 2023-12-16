2cc2e455e1892d324026b79a5091d6fe96d27006
ENES ERSÖZ <114816539+enesersoz@users.noreply.github.com>
2023-12-16 23:50:20 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,24 @@
+from main import ISO9660Reader
+from tkinter import filedialog, simpledialog
+
+class test:
+    if __name__ == "__main__":
+        # Create an instance of ISO9660Reader by selecting an ISO file with a GUI
+        iso_reader = ISO9660Reader.select_iso_file()
+
+        # Print volume descriptors
+        iso_reader.print_volume_descriptor("Standard", iso_reader.read_volume_descriptor(16 * 2048))
+        iso_reader.print_volume_descriptor("Joliet", iso_reader.read_volume_descriptor(17 * 2048))
+
+        # List all files and directories in the root directory
+        iso_reader.list_contents("")
+
+        # List contents of a specific directory
+        # Ask the user to select a file for extraction
+        file_to_extract = ISO9660Reader.select_file()
+        destination_folder = filedialog.askdirectory(title="Select Destination Folder")
+
+        # Extract a file from the ISO to the specified destination
+        iso_reader.extract_file(file_path=file_to_extract, destination=destination_folder)
+
+        print(f"ISO Path: {iso_reader.iso_path}")
